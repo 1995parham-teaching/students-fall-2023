f5a1fcae724a3d18bcff6fa15cb4404942e998aa
Parham Alvani <parham.alvani@gmail.com>
2023-11-05 18:02:33 +0000

    feat: update student model

--- internal/domain/model/student.go
+++ internal/domain/model/student.go
@@ -1,8 +1,9 @@
 package model
 
 type Student struct {
-	FirstName     string `json:"first_name,omitempty"`
-	LastName      string `json:"last_name,omitempty"`
-	EnteranceYear int    `json:"enterance_year,omitempty"`
-	Courses       []Course
+	ID           uint64   `json:"id,omitempty"`
+	FirstName    string   `json:"first_name,omitempty"`
+	LastName     string   `json:"last_name,omitempty"`
+	EntranceYear int      `json:"entrance_year,omitempty"`
+	Courses      []Course `json:"courses,omitempty"`
 }
