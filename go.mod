14ffd257ee76744b25a447dabaa77df3b04d7cd8
Parham Alvani <parham.alvani@gmail.com>
2023-11-14 15:10:52 +0000

    feat: add gorm-based implementation for students model

--- go.mod
+++ go.mod
@@ -11,14 +11,19 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-sqlite3 v1.14.18 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.15.0 // indirect
 	golang.org/x/net v0.18.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gorm.io/driver/sqlite v1.5.4 // indirect
+	gorm.io/gorm v1.25.5 // indirect
 )
---
+++ internal/infra/repository/studentsql/student.go
@@ -0,0 +1,76 @@
+package studentsql
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/1995parham-teaching/students-fall-2023/internal/domain/model"
+	"github.com/1995parham-teaching/students-fall-2023/internal/domain/repository/studentrepo"
+	"gorm.io/gorm"
+)
+
+type StudentDTO struct {
+	model.Student
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+type Repository struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) *Repository {
+	return &Repository{
+		db: db,
+	}
+}
+
+func (r *Repository) Add(ctx context.Context, model model.Student) error {
+	tx := r.db.WithContext(ctx).Create(StudentDTO{Student: model})
+	if tx.Error != nil {
+		if errors.Is(tx.Error, gorm.ErrDuplicatedKey) {
+			return studentrepo.ErrStudentIDDuplicate
+		}
+
+		return tx.Error
+	}
+
+	return nil
+}
+
+// nolint: cyclop
+func (r *Repository) Get(_ context.Context, cmd studentrepo.GetCommand) []model.Student {
+	var studentDTOs []StudentDTO
+
+	var condition StudentDTO
+
+	if cmd.ID != nil {
+		condition.ID = *cmd.ID
+	}
+
+	if cmd.FirstName != nil {
+		condition.FirstName = *cmd.FirstName
+	}
+
+	if cmd.LastName != nil {
+		condition.LastName = *cmd.LastName
+	}
+
+	if cmd.EntranceYear != nil {
+		condition.EntranceYear = *cmd.EntranceYear
+	}
+
+	if err := r.db.Where(&condition).Find(&studentDTOs); err != nil {
+		return nil
+	}
+
+	students := make([]model.Student, len(studentDTOs))
+
+	for index, dto := range studentDTOs {
+		students[index] = dto.Student
+	}
+
+	return students
+}
