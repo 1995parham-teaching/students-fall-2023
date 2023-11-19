ed3dd3aea1abe727aab639deb755225a100c6761
Parham Alvani <parham.alvani@gmail.com>
2023-11-19 04:00:27 +0000

    feat: update main to use gorm

--- main.go
+++ main.go
@@ -3,13 +3,25 @@ package main
 import (
 	"log"
 
+	"github.com/1995parham-teaching/students-fall-2023/internal/domain/model"
 	"github.com/1995parham-teaching/students-fall-2023/internal/domain/repository/studentrepo"
 	"github.com/1995parham-teaching/students-fall-2023/internal/infra/http/handler"
 	"github.com/1995parham-teaching/students-fall-2023/internal/infra/repository/studentmem"
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
+	db, err := gorm.Open(sqlite.Open("students.db"), new(gorm.Config))
+	if err != nil {
+		log.Fatalf("failed to connect database %v", err)
+	}
+
+	if err := db.AutoMigrate(new(model.Student), new(model.Course)); err != nil {
+		log.Fatalf("failed to run migrations %v", err)
+	}
+
 	app := echo.New()
 
 	var repo studentrepo.Repository = studentmem.New()
@@ -18,6 +30,6 @@ func main() {
 	h.Register(app.Group("students/"))
 
 	if err := app.Start("0.0.0.0:1373"); err != nil {
-		log.Fatal("server failed to start")
+		log.Fatalf("server failed to start %v", err)
 	}
 }
