56435d7389490e5ffd155cbe87f770f747768cb2
Parham Alvani <parham.alvani@gmail.com>
2026-01-14 19:13:32 +0000

    chore: use global validator object

--- internal/infra/http/request/student.go
+++ internal/infra/http/request/student.go
@@ -6,14 +6,15 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
+// nolint: gochecknoglobals
+var validate = validator.New(validator.WithRequiredStructEnabled())
+
 type StudentCreate struct {
 	Name   string `json:"name,omitempty"   validate:"required,alphaunicode"`
 	Family string `json:"family,omitempty" validate:"required,alphaunicode"`
 }
 
 func (sc StudentCreate) Validate() error {
-	validate := validator.New(validator.WithRequiredStructEnabled())
-
 	if err := validate.Struct(sc); err != nil {
 		return fmt.Errorf("create request validation failed %w", err)
 	}
